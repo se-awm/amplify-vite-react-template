74ee365293f090b0ef6ac47560b08083adde5a1d
rk-cache <rk@cachetechio.in>
2025-01-27 19:36:36 +0530

    Chnages in amplify yml

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <main>
-      <h1>React todos ${</h1>
+      <h1>React todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
