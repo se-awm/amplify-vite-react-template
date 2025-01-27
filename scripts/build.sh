09443ea343e6fe56600fc04cb05333e3abb0c8a1
rk-cache <rk@cachetechio.in>
2025-01-27 20:23:25 +0530

    test 4: rollback testing with error

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,9 @@ npm run build
 
 # Check if the build command failed
 if [ $? -ne 0 ]; then
-  echo "Build failed, but continuing to postBuild"
-  export BUILD_FAILED=true  # Set the environment variable for failure
-  exit 0  # Exit with 0 to allow the pipeline to continue
+  echo "Build failed"
+  exit 1  # Return a non-zero exit code if build fails
 else
   echo "Build succeeded"
-  export BUILD_FAILED=false  # Set the environment variable for success
+  exit 0  # Return 0 exit code if build succeeds
 fi
