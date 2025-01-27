c0373c84bc53f883555582f30c6130d8c844523d
rk-cache <rk@cachetechio.in>
2025-01-27 20:10:27 +0530

    some changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,9 @@ npm run build
 # Check if the build command failed
 if [ $? -ne 0 ]; then
   echo "Build failed, but continuing to postBuild"
+  export BUILD_FAILED=true  # Set the environment variable for failure
   exit 0  # Exit with 0 to allow the pipeline to continue
 else
   echo "Build succeeded"
+  export BUILD_FAILED=false  # Set the environment variable for success
 fi
